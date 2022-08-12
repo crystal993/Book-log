f3a7998fa9b6a0104ff7b11a3a9710128f8f98d1
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-12 23:38:22 +0900

    add TestPage

---
+++ src/components/comments/TestPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AddComment from "./AddComment";
+import Comment from "./Comment";
+
+function TestPage() {
+  return (
+    <>
+      <AddComment />
+      <Comment />
+    </>
+  );
+}
+
+export default TestPage;
