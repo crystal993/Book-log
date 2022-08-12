03ad96809ac302c83697814a74816739abffdcd6
Kim-wonder <glow2myself@gmail.com>
2022-08-12 23:57:32 +0900

    first

--- src/components/posts/AddForm.js
+++ src/components/posts/AddForm.js
@@ -2,6 +2,9 @@ import React from "react";
 
 function AddForm() {
   return <div>AddForm</div>;
+  //
 }
 
 export default AddForm;
+
+
--- src/pages/posts/AddPost.js
+++ src/pages/posts/AddPost.js
@@ -4,5 +4,5 @@ import Layout from "../../components/common/Layout";
 function AddPost() {
   return <Layout></Layout>;
 }
-
+//
 export default AddPost;
