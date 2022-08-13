704cf0e567f86f3b27162607518d80f0b8faead7
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-13 14:31:10 +0900

    파일명 변경

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fort";
+import styled from "styled-components";
 
-function Button() {
-  return <div>Button</div>;
+function Button({ content, icon, size, onClick }) {
+  return (
+    <Wrapper onClick={onClick}>
+      {icon && <Icon icon={icon} size={size} />}
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div``;
+const Icon = styled(FontAwesomeIcon)``;
 
 export default Button;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/common/Header";
+import Layout from "../components/common/Layout";
+import CardList from "../components/main/CardList";
+
+function Home() {
+  return (
+    <Layout>
+      <Header></Header>
+      <CardList />
+    </Layout>
+  );
+}
+
+export default Home;
