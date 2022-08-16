861dc1cd6d2fa6270001a7506a72f91f290755da
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-16 09:51:02 +0900

    회원가입 실시간 유효성 검사 - 옳은 조건일 때 메세지 추가

--- src/components/user/SignUpForm.jsx
+++ src/components/user/SignUpForm.jsx
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import Button from "../common/Button";
+import { nickCheck, passwordCheck, idCheck } from "../../shared/regex";
 
 function SignUpForm() {
   const { id } = useParams(); //postId
@@ -15,6 +16,7 @@ function SignUpForm() {
     handleSubmit,
     setFocus,
     reset,
+    watch,
     formState: { isDirty, errors },
   } = useForm({
     mode: "onChange",
@@ -22,13 +24,24 @@ function SignUpForm() {
 
   const onSubmitHandler = async (formData) => {};
 
+  const onIdCheckHandler = async (formData) => {};
+
+  const onNickCheckHandler = async (formData) => {};
+
   useEffect(() => {
     setFocus("account");
   }, []);
 
+  //TODO 회원가입 기능, 아이디 중복, 닉네임 중복체크
   return (
     <SignUpView>
-      <Form onSubmit={handleSubmit(onSubmitHandler)}>
+      <Form
+        onSubmit={handleSubmit(
+          onSubmitHandler,
+          onIdCheckHandler,
+          onNickCheckHandler
+        )}
+      >
         <Title>Sign Up</Title>
         <Container>
           <Label>ID</Label>
@@ -38,70 +51,138 @@ function SignUpForm() {
               placeholder="아이디"
               {...register("account", {
                 required: "아이디는 필수 입력사항입니다.",
+                validate: (value) => {
+                  const result = idCheck(value);
+                  if (!result) {
+                    return (
+                      <ErrorMsg>
+                        {"아이디는 숫자 또는 영문으로 6~12자 이내입니다."}
+                      </ErrorMsg>
+                    );
+                  } else {
+                    return (
+                      <CorrectMsg>
+                        {"조건에 일치하는 아이디 형식 입니다."}
+                      </CorrectMsg>
+                    );
+                  }
+                },
               })}
             />
             <Button content={"check"} />
           </InputBox>
           <ButtonBox>
-            <ErrorMsg>
-              {errors.account && (
-                <small role="alert">{errors.userid.message}</small>
-              )}
-            </ErrorMsg>
+            <MsgBox>
+              {errors.account && <ErrorMsg>{errors.account.message}</ErrorMsg>}
+            </MsgBox>
           </ButtonBox>
           <Label>PW</Label>
           <InputBox>
             <Input
-              type="text"
+              type="password"
               placeholder="비밀번호"
               {...register("password", {
                 required: "비밀번호는 필수 입력사항입니다.",
+                validate: (value) => {
+                  const result = passwordCheck(value);
+                  if (!result) {
+                    return (
+                      <ErrorMsg>
+                        {"비밀번호는 숫자 또는 영문으로 6~12자 이내입니다."}
+                      </ErrorMsg>
+                    );
+                  } else {
+                    return (
+                      <CorrectMsg>
+                        {"조건에 일치하는 비밀번호 형식 입니다."}
+                      </CorrectMsg>
+                    );
+                  }
+                },
               })}
             />
           </InputBox>
           <ButtonBox>
-            <ErrorMsg>
+            <MsgBox>
               {errors.password && (
-                <small role="alert">{errors.password.message}</small>
+                <ErrorMsg>{errors.password.message}</ErrorMsg>
               )}
-            </ErrorMsg>
+            </MsgBox>
           </ButtonBox>
           <Label>PW CHECK</Label>
           <InputBox>
             <Input
-              type="text"
+              type="password"
               placeholder="비밀번호"
               {...register("passwordCheck", {
                 required: "비밀번호는 필수 입력사항입니다.",
+                validate: (value) => {
+                  const result = passwordCheck(value);
+                  if (!result) {
+                    return (
+                      <ErrorMsg>
+                        {"비밀번호는 숫자 또는 영문으로 6~12자 이내입니다."}
+                      </ErrorMsg>
+                    );
+                  } else if (watch("password") !== value) {
+                    return (
+                      <ErrorMsg>{"비밀번호가 일치하지 않습니다."}</ErrorMsg>
+                    );
+                  } else if (
+                    watch("password") === value &&
+                    value.length !== 0
+                  ) {
+                    return <CorrectMsg>{"비밀번호가 일치합니다"}</CorrectMsg>;
+                  }
+                },
               })}
             />
           </InputBox>
           <ButtonBox>
-            <ErrorMsg>
+            <MsgBox>
               {errors.passwordCheck && (
-                <small role="alert">{errors.passwordCheck.message}</small>
+                <ErrorMsg>{errors.passwordCheck.message}</ErrorMsg>
               )}
-            </ErrorMsg>
+            </MsgBox>
           </ButtonBox>
           <Label>NICKNAME</Label>
           <InputBox>
             <Input
               type="text"
               placeholder="닉네임"
-              {...register("nickname ", {
+              {...register("nickname", {
                 required: "닉네임은 필수 입력사항입니다.",
+                validate: (value) => {
+                  const result = nickCheck(value);
+                  if (!result) {
+                    return (
+                      <ErrorMsg>
+                        {
+                          "닉네임은 특수문자를 제외한 문자, 숫자로 3~8자 이내입니다."
+                        }
+                      </ErrorMsg>
+                    );
+                  } else {
+                    return (
+                      <CorrectMsg>
+                        {"조건에 일치하는 닉네임 형식입니다."}
+                      </CorrectMsg>
+                    );
+                  }
+                },
               })}
             />
+            {/* value: /^[a-zA-Z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣]^{3,8}/i, */}
             <Button content={"check"} />
           </InputBox>
           <ButtonBox>
-            <ErrorMsg>
-              {errors.passwordCheck && (
-                <small role="alert">{errors.nickname.message}</small>
+            <MsgBox>
+              {errors.nickname && (
+                <ErrorMsg>{errors.nickname.message}</ErrorMsg>
               )}
-            </ErrorMsg>
+            </MsgBox>
           </ButtonBox>
-          <Label>IMAGE URL</Label>
+          <Label>PROFILE IMG</Label>
           <InputBox>
             <Input
               type="file"
@@ -111,11 +192,11 @@ function SignUpForm() {
             />
           </InputBox>
           <ButtonBox>
-            <ErrorMsg>
-              {errors.passwordCheck && (
-                <small role="alert">{errors.imageUrl.message}</small>
+            <MsgBox>
+              {errors.imageUrl && (
+                <ErrorMsg>{errors.imageUrl.message}</ErrorMsg>
               )}
-            </ErrorMsg>
+            </MsgBox>
           </ButtonBox>
         </Container>
         <Button type="submit" content={"회원가입"} />
@@ -209,11 +290,22 @@ const ButtonBox = styled.div`
   margin-right: -20px;
 `;
 
-const ErrorMsg = styled.div`
-  width: 70%;
-  color: ${(props) => props.theme.errorTextColor};
+const MsgBox = styled.div`
+  margin-top: -20px;
+  margin-bottom: 10px;
+  height: 50px;
 `;
 
 const Label = styled.label`
   color: ${({ theme }) => theme.subColor};
+`;
+
+const CorrectMsg = styled.p`
+  font-size: 12px;
+  color: ${({ theme }) => theme.mainColor};
+`;
+
+const ErrorMsg = styled.p`
+  font-size: 12px;
+  color: ${({ theme }) => theme.errorTextColor};
 `;
